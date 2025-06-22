60164d703350929140b41f658036ca66fba91b34
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2025-06-23 00:56:06 +0200

    add results

--- run.sh
+++ run.sh
@@ -1,18 +1,28 @@
+#!/bin/bash
 
 iid_start=2
-iid_end=2
-pids=(4)
+iid_end=3
+pids=(1 3 4 5 16 23)
 bchm_id_start=0
 bchm_id_end=13
 
-# 遍历所有参数组合
+MAX_CONCURRENT=10
+
+count=0
+
 for ((iid=$iid_start; iid<=$iid_end; iid++)); do
     for pid in "${pids[@]}"; do
         for ((bchm_id=$bchm_id_start; bchm_id<=$bchm_id_end; bchm_id++)); do
-            echo "Running: python discovery.py $iid $pid $bchm_id"
-            nohup python discovery.py $iid $pid $bchm_id &
+            if [ $count -ge $MAX_CONCURRENT ]; then
+                wait -n
+                count=$((count-1))
+            fi
+            echo "Starting: python discovery.py $iid $pid $bchm_id"
+            python discovery.py $iid $pid $bchm_id &
+            count=$((count+1))
         done
     done
 done
 
+wait
 echo "All commands have been executed."
