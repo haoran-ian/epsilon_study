52b3e6d2f589474f0d8de4a322e52888365649bb
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2025-07-02 01:12:27 +0200

    add results

---
+++ postprocess.py
@@ -0,0 +1,89 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.gridspec import GridSpec
+from matplotlib.patches import Rectangle
+
+instance_ids = [2, 3, 4, 5]
+problem_ids = [1, 3, 4, 5, 16, 23]
+epsilons = [0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.5, ""]
+bchms = ["beta", "expC_B", "expC_R", "expC_T", "mahalanobis", "midB",
+         "midT", "mir", "sat", "tor", "unif", "vectB", "vectR", "vectT"]
+labels = ["it", "pop_size", "best", "error", "prob_infeas",
+          "genInfeasibleElement", "genMutatedComponent", "ratio",
+          "meanImprovements", "meanImprovementsMut", "varPop", "avgF", "stdF",
+          "avgCR", "stdCR", "extension", "density", "shape", "eccentricity",
+          "dist_to_opt", "kl_unif"]
+
+
+def build_GTO(iid, pid, epsilon, bchm):
+    edge_types = ["", "-->", "o-o"]
+    graph_GTO = [[[0 for _ in range(3)] for _ in range(21)] for _ in range(21)]
+    for run in range(10):
+        f_path = f"data/graphs/{iid}_{pid}_{epsilon}_{bchm}_{run}.npy"
+        if not os.path.exists(f_path):
+            continue
+        graph = np.load(f_path)
+        for i in range(graph.shape[0]):
+            for j in range(graph.shape[1]):
+                for k in range(graph.shape[2]):
+                    graph_GTO[i][j][edge_types.index(graph[i][j][k])] += 1
+    return graph_GTO
+
+
+def plot_GTO(graph, iid, pid, epsilon, bchm):
+    data_normalized = graph / graph.sum(axis=2, keepdims=True)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.set_xlim(0, 21)
+    ax.set_ylim(0, 21)
+    ax.set_aspect("equal")
+    ax.invert_yaxis()
+    ax.set_xticks(np.arange(0, 21, 1))
+    ax.set_yticks(np.arange(0, 21, 1))
+    ax.grid(True, linestyle="-", linewidth=0.5, color="gray")
+    colors = ["#556270", "#FF6B6B", "#4ECDC4"]
+    for i in range(21):
+        for j in range(21):
+            values = data_normalized[i, j]
+            x_start = j
+            y_start = i
+            width_accum = 0
+            for k in range(3):
+                width = values[k]
+                rect = Rectangle((x_start + width_accum, y_start),
+                                 width,
+                                 1,
+                                 facecolor=colors[k],
+                                 edgecolor="none")
+                ax.add_patch(rect)
+                width_accum += width
+    
+    ax.set_xticks(np.arange(0.5, 21.5, 1))  # 将标签放在方格中心
+    ax.set_xticklabels(labels, rotation=45, ha='right', fontsize=9)
+
+    # 设置Y轴刻度位置和标签
+    ax.set_yticks(np.arange(0.5, 21.5, 1))
+    ax.set_yticklabels(labels, fontsize=9)
+    from matplotlib.patches import Patch
+    legend_elements = [
+        Patch(facecolor=colors[0], label="no edge"),
+        Patch(facecolor=colors[1], label="-->"),
+        Patch(facecolor=colors[2], label="o-o")
+    ]
+    ax.legend(handles=legend_elements, loc="upper right")
+    plt.title(f"iid: {iid}, fid: {pid}, epsilon: {epsilon}, bchm: {bchm}")
+    plt.tight_layout()
+    plt.savefig(f"results/graph_GTO/{iid}_{pid}_{epsilon}_{bchm}.png")
+    plt.cla()
+
+
+for iid in instance_ids:
+    for pid in problem_ids:
+        for epsilon in epsilons:
+            for bchm in bchms:
+                graph_GTO = build_GTO(iid, pid, epsilon, bchm)
+                graph_GTO = np.array(graph_GTO)
+                if np.sum(graph_GTO) == 0:
+                    continue
+                plot_GTO(graph_GTO, iid, pid, epsilon, bchm)
---
+++ time_series_discovery.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+# plt.style.use('ggplot')
+import sklearn
+
+import tigramite
+from tigramite import data_processing as pp
+from tigramite.toymodels import structural_causal_processes as toys
+
+from tigramite import plotting as tp
+from tigramite.pcmci import PCMCI
+from tigramite.lpcmci import LPCMCI
+
+from tigramite.independence_tests.parcorr import ParCorr
+from tigramite.independence_tests.robust_parcorr import RobustParCorr
+from tigramite.independence_tests.parcorr_wls import ParCorrWLS
+from tigramite.independence_tests.gpdc import GPDC
+from tigramite.independence_tests.cmiknn import CMIknn
+from tigramite.independence_tests.cmisymb import CMIsymb
+from tigramite.independence_tests.gsquared import Gsquared
+from tigramite.independence_tests.regressionCI import RegressionCI
+
+instance_ids = [2, 3, 4, 5]
+problem_ids = [1, 3, 4, 5, 16, 23]
+epsilons = [0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.5, ""]
+bchms = ["beta", "expC_B", "expC_R", "expC_T", "mahalanobis", "midB",
+         "midT", "mir", "sat", "tor", "unif", "vectB", "vectR", "vectT"]
+
+
+def build_atom_data(iid, pid, bchm, eps):
+    dfs = []
+    for run in range(1, 11):
+        f_path = f"data/instance_{iid}/LSHADE_{bchm}_f{pid}_D20_eps{eps}run{run}_gen.csv"
+        if os.path.exists(f_path):
+            df = pd.read_csv(f_path)
+            first_col = df.columns[0]
+            df = df[df[first_col] != 0.0]
+            df.drop("kl_beta", axis=1, inplace=True)
+            df = df.select_dtypes(exclude=['object'])
+            dataframe = pp.DataFrame(df.values,
+                                     datatime={0: np.arange(len(df))},
+                                     var_names=df.columns)
+            print(df.columns)
+            dfs += [dataframe]
+        else:
+            print(f"Missing file: {f_path}")
+    return dfs, df.columns
+
+
+def discovery(dataframe, var_names):
+    parcorr = ParCorr(significance='analytic')
+    pcmci = PCMCI(
+        dataframe=dataframe,
+        cond_ind_test=parcorr,
+        verbosity=1)
+    correlations = pcmci.get_lagged_dependencies(
+        tau_max=20, val_only=True)['val_matrix']
+    pcmci.verbosity = 1
+    results = pcmci.run_pcmci(tau_max=8, pc_alpha=None, alpha_level=0.01)
+    q_matrix = pcmci.get_corrected_pvalues(
+        p_matrix=results['p_matrix'], tau_max=8, fdr_method='fdr_bh')
+    pcmci.print_significant_links(
+        p_matrix=q_matrix,
+        val_matrix=results['val_matrix'],
+        alpha_level=0.01)
+    graph = pcmci.get_graph_from_pmatrix(p_matrix=q_matrix, alpha_level=0.01,
+                                         tau_min=0, tau_max=8, link_assumptions=None)
+    results['graph'] = graph
+    return graph
+
+
+for iid in instance_ids:
+    for pid in problem_ids:
+        for epsilon in epsilons:
+            for bchm in bchms:
+                dfs, var_names = build_atom_data(iid, pid, bchm, epsilon)
+                for i in range(len(dfs)):
+                    graph = discovery(dfs[i], var_names)
+                    np.save(
+                        f"data/graphs/{iid}_{pid}_{epsilon}_{bchm}_{i}.npy", graph)
